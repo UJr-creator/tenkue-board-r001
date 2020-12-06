82dd7d3bc00f39098176b15e9c91314fbcb43f3b
Ueno Ai <skyai@Ueno-no-MacBook-Air.local>
2020-12-06 17:27:26 +0900

    post.rbの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user,
-  optional: true 
-  validates :content, {presence: true, length: {maximum:140}}
-  validates :user_id,{presence:true}
+  validates :content, presence: true, length: {maximum:140}
+  validates :user_id,presence:true
 
   def user
     return User.find_by(id: self.user_id)
   end
-
 end
 
 
