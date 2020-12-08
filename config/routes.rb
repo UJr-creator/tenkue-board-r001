bb6e0461acc055786cafeb9afe17af87de1d76cd
UJr-creator <rokumugi123good@gmail.com>
2020-12-08 22:36:02 +0900

    clean:コードインデントの可読性向上

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  # get "posts/index" => "posts#index"
-  # get "posts/new" => "posts#new"
-  # get "posts/:id" => "posts#show"
-  # post "posts/create" => "posts#create"
-  # get "posts/:id/edit" => "posts#edit"
-  # post "posts/:id/destroy" => "posts#destroy"
-
-  
-
   resources :posts
 
   devise_for :users, controllers: {
