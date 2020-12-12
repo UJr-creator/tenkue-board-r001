bb8e217aa0798a34bb1f9d5ca1a7313186458339
新田陸人 <>
2020-12-12 20:40:20 +0900

    いいねした時のページ遷移先を直前のページに指定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,13 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(user_id: current_user.id, post_id: @post.id)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @like = current_user.likes.find_by(post_id: @post.id)
     @like.destroy
+    redirect_back(fallback_location: root_path)
   end
 
   private
