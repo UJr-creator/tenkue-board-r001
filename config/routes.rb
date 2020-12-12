80c3105015d904d2c16193902d36f3d7a2a3e18b
UJr-creator <rokumugi123good@gmail.com>
2020-12-12 13:42:37 +0900

    コメント投稿画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'comments/create'
   root to: "posts#index"
   resources :posts do
-    post "comments", to:"comments#create"
+    post "comments", to: "comments#create"
   end
 
   devise_for :users, controllers: {
