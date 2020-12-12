d4546acff8edcac966ad08c72793d3fde57cede7
UJr-creator <rokumugi123good@gmail.com>
2020-12-12 11:51:50 +0900

    feat:commentstコントローラー作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   root to: "posts#index"
   resources :posts
 
