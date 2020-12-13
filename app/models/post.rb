2a2328a595c78dee058181a0b429b620cbb97170
新田陸人 <>
2020-12-13 19:25:06 +0900

    不要なアソシエーションを削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
-  has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true, length: {maximum:140}
   validates :user_id,presence:true
 
@@ -12,6 +11,6 @@ class Post < ApplicationRecord
   def user
     return User.find_by(id: self.user_id)
   end
-  
+
   has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :liked_posts, through: :likes, source: :post
   has_many :comments
 
   def self.guest
