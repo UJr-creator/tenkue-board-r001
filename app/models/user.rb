342232d659c6feba9bf1f184bc8cbc35e754102d
UJr-creator <rokumugi123good@gmail.com>
2020-12-13 20:11:12 +0900

    Userモデル　バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
+  validates :email, format: { with: /[ -~｡-ﾟ]/ }
+  validates :password, format: { with: /[0-9a-zA-Z]{8,32}$+/ }
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザ') do |user|
       user.password = SecureRandom.urlsafe_base64
