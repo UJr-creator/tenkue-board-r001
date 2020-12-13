bbd5e56c7826f88369db24c15b3d702643ba9c0c
UJr-creator <rokumugi123good@gmail.com>
2020-12-13 18:00:57 +0900

    Postモデル正規表現記載

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true, length: {maximum:140}
+  validates :content, format: { with: /\A[[:^cntrl:]]{1,10}$+/ }
   validates :user_id,presence:true
 
   def user
