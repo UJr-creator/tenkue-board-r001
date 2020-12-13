2f5c7f2f3254893651a6eea40ab10c1e3386e786
UJr-creator <rokumugi123good@gmail.com>
2020-12-13 18:02:25 +0900

    fix:post_spec.rb修正

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Post, type: :model do
   it "is not null" do
     post = Post.new(content: nil)
     post.valid?
-    expect(post.errors[:content]).to include("を入力してください")
+    expect(post.errors[:content]).to include("は不正な値です")
   end
 
   it "投稿内容は140文字以内" do
     post = Post.new(content: "a" * 141)
     post.valid?
-    expect(post.errors[:content]).to include("は140文字以内で入力してください")
+    expect(post.errors[:content]).to include("は不正な値です")
   end
 end
