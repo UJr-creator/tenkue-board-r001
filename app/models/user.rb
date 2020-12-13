aebfa6d75e3740c7c0d98f70a51136c84bc69c0b
新田陸人 <>
2020-12-13 20:27:53 +0900

    post,user モデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :posts
+  has_many :likes
   has_many :comments
 
   def self.guest
